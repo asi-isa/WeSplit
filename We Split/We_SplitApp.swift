74a145570c371933de42488ce2965104c342532e
asi-isa <ali.inceoglu@protonmail.com>
2025-09-06 20:43:03 +0200

    Initial Commit

---
+++ We Split/We_SplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  We_SplitApp.swift
+//  We Split
+//
+//  Created by isa on 06.09.25.
+//
+
+import SwiftUI
+
+@main
+struct We_SplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
